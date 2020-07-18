96ef66a934cb0748f2cf28da2ece6ac43d502bc2
Andrew Ball <andy.bob.ball@gmail.com>
2020-07-18 17:39:11 -0500

    Updates

---
+++ precache-manifest.0996a9dbc786cf83e49c1c800b1b3b6e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "945d2a92cec0b60bcc6135c3fa77a840",
+    "url": "/AndrewBall/index.html"
+  },
+  {
+    "revision": "3d68b4e80154212b1db7",
+    "url": "/AndrewBall/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "59b0fcdb01821aec7058",
+    "url": "/AndrewBall/static/css/main.64f222bf.chunk.css"
+  },
+  {
+    "revision": "3d68b4e80154212b1db7",
+    "url": "/AndrewBall/static/js/2.c12ba3ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AndrewBall/static/js/2.c12ba3ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b0fcdb01821aec7058",
+    "url": "/AndrewBall/static/js/main.850a630a.chunk.js"
+  },
+  {
+    "revision": "7e0867de9ea974a88c90",
+    "url": "/AndrewBall/static/js/runtime-main.c208e679.js"
+  },
+  {
+    "revision": "a4542f7941669bd08910882f922d3dc0",
+    "url": "/AndrewBall/static/media/DiceRoller.a4542f79.png"
+  },
+  {
+    "revision": "76ac9e42d736b246287a0db21f2f39ed",
+    "url": "/AndrewBall/static/media/Gardenseedr.76ac9e42.png"
+  },
+  {
+    "revision": "d306d55c4295f8155d8085a47bbc2371",
+    "url": "/AndrewBall/static/media/Gradient.d306d55c.png"
+  },
+  {
+    "revision": "2774b33f68e5b29aaafbb4daabdcbf53",
+    "url": "/AndrewBall/static/media/IMG-6331.2774b33f.JPG"
+  },
+  {
+    "revision": "f1938d5f00318d5e506d259ee3dacfbe",
+    "url": "/AndrewBall/static/media/IMG_6032.f1938d5f.jpeg"
+  },
+  {
+    "revision": "cfad92b03cc4d8228ed84932a399e84a",
+    "url": "/AndrewBall/static/media/LaserLinks.cfad92b0.png"
+  },
+  {
+    "revision": "c91525afe3db73ce834557b3e481d03c",
+    "url": "/AndrewBall/static/media/MrLewis.c91525af.png"
+  },
+  {
+    "revision": "be64cedb2448396e66a4dc285003e721",
+    "url": "/AndrewBall/static/media/WatercolorSkull.be64cedb.jpg"
+  },
+  {
+    "revision": "9127a9650efcd522d64350b623439126",
+    "url": "/AndrewBall/static/media/WeatherMap.9127a965.png"
+  },
+  {
+    "revision": "0e4018f6fe4711674d1b9612e6a273d9",
+    "url": "/AndrewBall/static/media/bobbys-party.0e4018f6.png"
+  },
+  {
+    "revision": "5d573cacb0b8370e3d8ff3bc73b2b6d5",
+    "url": "/AndrewBall/static/media/red-bats.5d573cac.jpeg"
+  },
+  {
+    "revision": "18e932ca3e0c9e6ffe427a15412230f4",
+    "url": "/AndrewBall/static/media/skull-stamp.18e932ca.JPG"
+  },
+  {
+    "revision": "e04564db4098e24feb54780d1beeb010",
+    "url": "/AndrewBall/static/media/space-walk.e04564db.JPG"
+  }
+]);
