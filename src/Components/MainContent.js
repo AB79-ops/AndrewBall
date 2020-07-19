c129a7d0dc969eaecfe18939a09837a9be4a5e5d
Andrew Ball <andy.bob.ball@gmail.com>
2020-07-18 19:29:04 -0500

    headshot

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Headshot from "../images/Headshot-wide.jpeg"
 
 class MainContent extends React.Component{
     render() {
         return(
         <div className="mainContent">
+            <img style={{height: "400px", position:"relative", left: "400px"}} src={Headshot} alt="Headshot"/>
         </div>
         )
     }
