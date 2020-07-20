fb744929306b4ab8d6092b45b988fcb04080d4a2
Andrew Ball <andy.bob.ball@gmail.com>
2020-07-20 18:14:17 -0500

    styling

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
     render() {
         return (
             <footer className="footer">
-                <h5>Contact:</h5>
+                <h5>CONTACT</h5>
                 <h5>Email: <a href="andy.bob.ball@gmail.com">andy.bob.ball@gmail.com</a></h5>
                 <h5>LinkedIn: <a href="andy.bob.ball@gmail.com">Andrew-Robert-Ball</a></h5>
             </footer>
