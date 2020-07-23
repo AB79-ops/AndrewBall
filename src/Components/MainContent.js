58284661bdcdab3fce53121076c33f705da41787
Andrew Ball <andy.bob.ball@gmail.com>
2020-07-23 12:30:07 -0500

    fixed safari responsive issue

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -1,18 +1,16 @@
 import React from "react";
 import Headshot from "../images/Headshot-cropped.jpeg"
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
+// import Container from 'react-bootstrap/Container'
+// import Row from 'react-bootstrap/Row'
 
 
 class MainContent extends React.Component {
     render() {
         return (
-            <Container className="mainContent">
-                <Row>
-                    <img className="blurred-edge" style={{width: "60vw", position: "relative"}} src={Headshot}
+            <div className="mainContent">
+                    <img className="blurred-edge" width="60%" height="100%" style={{width: "60vw", position: "relative"}} src={Headshot}
                          alt="Headshot"/>
-                </Row>
-            </Container>
+            </div>
         )
     }
 }
