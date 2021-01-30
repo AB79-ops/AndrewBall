acb2cb943d53e207405918727bc385ed3b7deb3e
AndrewBall79 <andy.bob.ball@gmail.com>
2021-01-29 20:54:34 -0600

    building json

--- src/data/artProjectsData.js
+++ src/data/artProjectsData.js
@@ -2,23 +2,39 @@ const artProjectsData = [
     {
         id: 1,
         title: "Lenticular_Mars",
-        description: "Lines do no justice in these lost images from mars",
+        description: "Ink on paper",
+        price:"Maybe NFS, ask about prints",
         url: require("../artImages/Another Face On Mars.jpg"),
-        alt: "Inc0 Vctrd.",
+        alt: "Inc0 Lines do no justice in these lost images from mars Vctrd.",
     },
     {
         id: 2,
         title: "Diamond Eyes",
-        description: "Humbly Ruling All With A Skull",
+        description: "Ink on paper",
+        price:"Maybe NFS, ask about prints",
         url: require("../artImages/Diamond Eyes.jpg"),
-        alt: "Bejeweled Skull",
+        alt: "Humbly Ruling All With A Skull",
     },
     {
-        id: 2,
+        id: 3,
         title: "Finding You",
-        description: "Humbly Ruling All With A Skull",
-        url: require("../artImages/Diamond Eyes.jpg"),
-        alt: "Bejeweled Skull",
+        description: "Ink on paper",
+        url: require("../artImages/Finding You.jpg"),
+        alt: "Where we feel we've lost a winning battle",
+    },
+    {
+        id: 4,
+        title: "Grinn",
+        description: "Marker on paper",
+        url: require("../artImages/Grinn.jpg"),
+        alt: "Full C0l0r PsyCh0sis",
+    },
+    {
+        id: 5,
+        title: "Finding You",
+        description: "Ink on paper",
+        url: require("../artImages/Finding You.jpg"),
+        alt: "Where we feel we've lost a winning battle",
     }
 ];
 
