c0826f0af0e0dc1f17afa3f45f88c4da0b13f30a
AndrewBall79 <andy.bob.ball@gmail.com>
2021-01-29 20:34:57 -0600

    new art json

---
+++ src/data/artProjectsData.js
@@ -0,0 +1,25 @@
+const artProjectsData = [
+    {
+        id: 1,
+        title: "Lenticular_Mars",
+        description: "Lines do no justice in these lost images from mars",
+        url: require("../artImages/Another Face On Mars.jpg"),
+        alt: "Inc0 Vctrd.",
+    },
+    {
+        id: 2,
+        title: "Diamond Eyes",
+        description: "Humbly Ruling All With A Skull",
+        url: require("../artImages/Diamond Eyes.jpg"),
+        alt: "Bejeweled Skull",
+    },
+    {
+        id: 2,
+        title: "Finding You",
+        description: "Humbly Ruling All With A Skull",
+        url: require("../artImages/Diamond Eyes.jpg"),
+        alt: "Bejeweled Skull",
+    }
+];
+
+export default artProjectsData
