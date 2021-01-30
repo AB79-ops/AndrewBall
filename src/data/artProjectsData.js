30e6230c5edea8fe4b90fb85fa9cde43149b2ab5
AndrewBall79 <andy.bob.ball@gmail.com>
2021-01-29 21:02:33 -0600

    updating json

--- src/data/artProjectsData.js
+++ src/data/artProjectsData.js
@@ -19,6 +19,7 @@ const artProjectsData = [
         id: 3,
         title: "Finding You",
         description: "Ink on paper",
+        price:"Maybe NFS, ask about prints",
         url: require("../artImages/Finding You.jpg"),
         alt: "Where we feel we've lost a winning battle",
     },
@@ -26,15 +27,25 @@ const artProjectsData = [
         id: 4,
         title: "Grinn",
         description: "Marker on paper",
+        price:"Maybe NFS, ask about prints",
         url: require("../artImages/Grinn.jpg"),
         alt: "Full C0l0r PsyCh0sis",
     },
     {
         id: 5,
-        title: "Finding You",
+        title: "Batsssuck",
         description: "Ink on paper",
-        url: require("../artImages/Finding You.jpg"),
-        alt: "Where we feel we've lost a winning battle",
+        price:"Sold",
+        url: require("../artImages/Bats Suck.jpg"),
+        alt: "Bats, but they suck",
+    },
+    {
+        id: 6,
+        title: "Red Batsssuck Too",
+        description: "Ink and Acrylic on paper",
+        price:"Sold",
+        url: require("../artImages/red-bats.jpg"),
+        alt: "Red bats, and they also suck",
     }
 ];
 
